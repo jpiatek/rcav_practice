6446d7f356357e1dfe22ed90562e377b68c127ac
jpiatek <jpiatek@chicagobooth.edu>
2015-07-25 16:31:59 -0500

    completes

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -4,23 +4,34 @@ class CalculationsController < ApplicationController
   end
 
  def sq
-      @sq="1"
-      @output=@sq
+    @numbersq = params["numbersq"]
+      @output=@numbersq.to_i*@numbersq.to_i
     render("application.html.erb")
   end
+
     def sqrt
-      @sqrt="5"
-      @output=@sqrt
+      @numbersqrt = params["numbersqrt"]
+      @output=@numbersqrt.to_i**(0.5)
     render("application.html.erb")
   end
+
    def random
-      @random="4"
+
+      @min = params["min"]
+      @max = params["max"]
+      @random= @min.to_f + (rand()*(@max.to_f-@min.to_f))
       @output=@random
     render("application.html.erb")
   end
+
    def payment
-      @payment="9"
+      @basis = params["basis"]
+      @years = params["years"]
+      @loan = params["loan"]
+      @payment= ((@basis.to_f/10000/12)*(@loan.to_f))/(1-(1+@basis.to_f/10000/12)**-(@years.to_f*12))
       @output=@payment
+
+
     render("application.html.erb")
   end
 
